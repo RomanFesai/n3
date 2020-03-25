b273bacf76b87eedbc916ec793d5cbb367bf4cba
RomanFesai <57192146+RomanFesai@users.noreply.github.com>
2020-03-25 21:13:08 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,65 +1,111 @@
 <!DOCTYPE html>
 <html lang="ru">
-
-  <head>
-    <meta charset="utf-8"/>
-    <title>n3</title>
-      <link href="style.css" rel="stylesheet" />
-  </head>
-  
-<body>
-  <div class="navform">
-            <h2>Form<a id=“down”></a></h2>
-            <form action=""
-                  method="GET">
-                <label>
-                    Name:<br />
-                    <input name="Name"
-                           value="" />
-                </label><br />
-                <label>
-                    e-mail:<br />
-                    <input name="field-email"
-                           value=""
-                           type="email" />
-                </label><br />
-                <label>
-                    Birthday:<br />
-                    <input name="field-date"
-                           value="2019-08-13"
-                           type="text" />
-                </label><br />
-                <div>
-                    Number of limbs: <br />
-                    <input type="radio"
-                           name="Konechnosti"
-                           checked="checked"
-                           value="2-4"> 2-4 <br />
-                    <input type="radio"
-                           name="Konechnosti"
-                           value="4-8"> 4-8 <br />
-                    <input type="radio"
-                           name="Konechnosti"
-                           value="8 and more"> 8+ <br />
-                </div>
-                <div>
-                   	Superpowers:<br />
-                    <select name="Sverxsposobnosti" multiple="multiple">
-                        <option value="One">Multidimensionality</option>
-                        <option value="Two">Bending time</option>
-                        <option value="Three">Fundamental immortality </option>
-                    </select>
-                </div>
-                <div>
-                    Biography:<br />
-                    <textarea name="Biografy"></textarea>
-                </div>
-                <div>
-                    <input type="checkbox" checked="checked">Acquainted with the contract<br />
-                </div>
-                <p>
-                    <input type="submit" value="OK">
-                </p>
-            </form>
-        </div>
+<head>
+	<title>Web - 3</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body class="bg-primary">
+	<div class="jumbotron w-25 mx-auto my-5">
+		<form action="" method="POST">
+			<div class="from-group d-flex flex-column">
+				<div class="my-2">
+					<label>First Name</label>
+					<div class="col-sm-10">
+        				<input type="text" class="form-control" name="Name" placeholder="">
+      				</div>
+      			</div>
+      			<div class="my-2">
+      				<label>Email</label>
+      				<div class="col-sm-10">
+        				<input type="email" class="form-control" name="Email" aria-describedby="emailHelp" placeholder="">
+      				</div>
+      			</div>
+      			<div class="my-2">
+      				<label>Date of Birth</label>
+      				<div class="d-flex flex-row justify-content-around">
+      					<div class="d-flex flex-column">
+      						<label>Day</label>
+      						<input type="text" class="form-control" name="DD" placeholder="">
+      					</div>
+      					<div class="d-flex flex-column">
+      						<label>Month</label>
+      						<input type="text" class="form-control" name="DM" placeholder="">
+      					</div>
+      					<div class="d-flex flex-column">
+      						<label>Year</label>
+      						<input type="text" class="form-control" name="DY" placeholder="">
+      					</div>
+      				</div>
+      			</div>
+      			<div class="my-2">
+      				<label>Sex</label>
+      				<div class="form-check">
+        				<label class="form-check-label">
+          					<input type="radio" class="form-check-input" name="Rad" id="SMale" value="MALE" checked="checked">Male
+       					</label>
+      				</div>
+      				<div class="form-check">
+      					<label class="form-check-label">
+          					<input type="radio" class="form-check-input" name="Rad" id="SFe" value="FEMALE">Female
+        				</label>
+      				</div>
+      			</div>
+      			<div class="my-2">
+      				<label>Number of limbs</label>
+      				<div class="d-flex justify-content-between">
+	      				<div class="form-check">
+	        				<label class="form-check-label">
+	          					<input type="radio" class="form-check-input" name="Limbs" id="0" value="0" checked="checked">0
+	       					</label>
+	      				</div>
+	      				<div class="form-check">
+	      					<label class="form-check-label">
+	          					<input type="radio" class="form-check-input" name="Limbs" id="1" value="1">1
+	        				</label>
+	      				</div>
+	      				<div class="form-check">
+	      					<label class="form-check-label">
+	          					<input type="radio" class="form-check-input" name="Limbs" id="2" value="2">2
+	        				</label>
+	      				</div>
+	      				<div class="form-check">
+	      					<label class="form-check-label">
+	          					<input type="radio" class="form-check-input" name="Limbs" id="3" value="3">3
+	        				</label>
+	      				</div>
+	      				<div class="form-check">
+	      					<label class="form-check-label">
+	          					<input type="radio" class="form-check-input" name="Limbs" id="4" value="4">4
+	        				</label>
+	      				</div>
+	      			</div>
+      			</div>
+      			<div class="my-2">
+      				<div class="form-group">
+      					<label for="exampleSelect2">Superpowers</label>
+      					<select multiple="multiple" class="form-control" name="SP[]">
+        				<option>Great power</option>
+       					<option>Invisibility</option>
+				        <option>Absolute knowledge</option>
+				        <option>Fundamental immortality</option>
+				      </select>
+				    </div>
+      			</div>
+      			<div class="my-2">
+      				<div class="form-group">
+      					<label>Biography</label>
+      					<textarea class="form-control" name="BG" rows="3"></textarea>
+    				</div>	
+      			</div>
+      			<div class="my-2">
+      				<div class="form-check">
+        				<label><input class="form-check-input" type="checkbox" name="CH" value="Yes" checked="checked">I got acquainted with the contact
+        				</label>
+      				</div>
+      			</div>
+      			<button type="submit" class="btn btn-primary">Send</button>
+			</div>
+		</form>
+	</div>
 </body>
+</html>
